b2cff58be601410e11315d39d3b355167edcbdce
MichaelChoudhury <MichaelChoudhury@users.noreply.github.com>
2023-06-08 01:30:47 +0000

    final commit ep 06

--- 06-exploring-the-world/src/components/Body.js
+++ 06-exploring-the-world/src/components/Body.js
@@ -1,10 +1,14 @@
 import RestaurantCard from "./RestaurantCard";
 import { useEffect, useState } from "react";
-import resList from "../utils/mockData";
+//import resList from "../utils/mockData";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
   const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  console.log("Body Rendered");
 
   useEffect(() => {
     fetchData();
@@ -12,40 +16,57 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      `https://michaelchoudhury-bookish-acorn-r964pg5v5g2p4jj-3000.preview.app.github.dev/api/restaurants?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING`,
-      )
+      `https://michaelchoudhury-bookish-acorn-r964pg5v5g2p4jj-3001.preview.app.github.dev/api/restaurants?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING`,
+      );
 
   const json = await data.json();
 
+  console.log(json);
+
   setListOfRestaurants(json?.data?.cards[2]?.data?.data?.cards);
   setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+  };
 
-  console.log(json);
-  
-    };
-
-
-  console.log("Body Rendered");
-
-    return(
+    return listOfRestaurants.length === 0 ? <Shimmer /> : (
       <div className="body">
         <div className="filter">
-          <button
-            className="filter-btn"
-            onClick={() => {
-              const filteredList = listOfRestaurants.filter(
-                (res) => res.data.avgRating > 4.4
-              );
-              setListOfRestaurants(filteredList);
-              console.log(listOfRestaurants);
-
+          <div className="search">
+            <input 
+              type="text" 
+              className="search-box" 
+              value={searchText}
+              onChange={(e) => {
+              setSearchText(e.target.value);
             }}
-            >
+             />
+            <button onClick={() => {
+
+              console.log(searchText);
+
+              const filteredRestaurants = listOfRestaurants.filter(
+                (res) => res.data.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+
+              setFilteredRestaurants(filteredRestaurants);
+
+            }}>Search</button>
+          </div>
+             <button
+            className="filter-btn"
+             onClick={() => {
+               const filteredRestaurants = listOfRestaurants.filter(
+                 (res) => res.data.avgRating > 4
+               );
+               setFilteredRestaurants(filteredRestaurants);
+               console.log(listOfRestaurants);
+ 
+             }}
+             >
               Top Rated Restaurants
           </button>
         </div>
         <div className="res-container">
-            {listOfRestaurants.map((restaurant) => (
+            {filteredRestaurants.map((restaurant) => (
               <RestaurantCard key={restaurant.data.id} resData={restaurant} />
             ))}
         </div>
--- server/server.js
+++ server/server.js
@@ -4,20 +4,22 @@ const fetch = require('cross-fetch');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
 
 //app.use(bodyParser.json());
-app.use(cors(), bodyParser.json());
+//app.use(cors(), bodyParser.json());
+
+app.use(cors());
 
 app.get('/', (req, res) => { 
     res.json({"test":"hello instafood lovers !!! "});
   })
 
-app.get('/api/restaurants', (req, res) => {
+  app.get('/api/restaurants', (req, res) => {
     const { lat, lng } = req.query;
     console.log(req.query);
     const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&page_type=DESKTOP_WEB_LISTING`;
