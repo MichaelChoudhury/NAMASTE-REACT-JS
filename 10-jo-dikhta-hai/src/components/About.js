0a84831acc24457ed39602d25d7846ae7c8075cd
MichaelChoudhury <MichaelChoudhury@users.noreply.github.com>
2023-07-01 03:15:47 +0000

    initial commit ep. 10

---
+++ 10-jo-dikhta-hai/src/components/About.js
@@ -0,0 +1,45 @@
+import { Component } from "react";
+import User from "./User";
+import UserClass from "./UserClass";
+
+class About extends Component {
+    constructor(props) {
+        super(props);
+
+        console.log("Parent Constructor");
+    }
+
+    componentDidMount() {
+        console.log("Parent Component Did Mount");
+    }
+
+    render() {
+        console.log("Parent Render");
+        return(
+            <div>
+                <h1>About Class Component</h1>
+                <h2>This is Namaste React Web Series</h2>
+                {/* <User name={"Michael Choudhury (function)"}/> */}
+    
+                <UserClass name={"First"} location={"Brisbane Class"} />
+                {/* <UserClass name={"Second"} location={"Austin Class"} /> */}
+            </div>
+        );
+    }
+}
+
+
+
+// const About = () => {
+//     return(
+//         <div>
+//             <h1>About Page</h1>
+//             <h2>This is Namaste React Web Series</h2>
+//             {/* <User name={"Michael Choudhury (function)"}/> */}
+
+//             <UserClass name={"Michael Choudhury (class)"} location={"Brisbane Class"} />
+//         </div>
+//     );
+// };
+
+export default About;
