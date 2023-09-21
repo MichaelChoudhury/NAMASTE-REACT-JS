934bbbb250066b5d02bf86e7c868cd5f836e1f26
MichaelChoudhury <MichaelChoudhury@users.noreply.github.com>
2023-09-21 12:31:29 +0000

    ep 12

---
+++ 12-lets-build-our-store/src/utils/constants.js
@@ -0,0 +1,9 @@
+export const CDN_URL = 
+    "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
+
+export const LOGO_URL = 
+    "https://as2.ftcdn.net/v2/jpg/02/88/84/21/1000_F_288842168_nhgjfMO0vtARTs6obR3i9bfdRIuaSL56.jpg"
+
+export const MENU_API =
+`https://michaelchoudhury-bookish-acorn-r964pg5v5g2p4jj-3003.app.github.dev/api/menu?/page-type=REGULAR_MENU&complete-menu=true&lat=22.572646&lng=88.36389500000001&restaurantId=`
+
